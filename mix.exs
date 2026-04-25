80b1efb02be01b6428236974d761b0ad2e20e29f
andyl <andy@r210.com>
2026-04-25 16:10:20 -0700

    fix: add git_ops

--- mix.exs
+++ mix.exs
@@ -1,10 +1,11 @@
 defmodule PhoenixLite.MixProject do
   use Mix.Project
 
+  @version "0.0.1"
   def project do
     [
       app: :phoenix_lite,
-      version: "0.0.1",
+      version: @version,
       elixir: "~> 1.19",
       start_permanent: Mix.env() == :prod,
       deps: deps()
@@ -19,8 +20,9 @@ defmodule PhoenixLite.MixProject do
 
   defp deps do
     [
+      {:git_ops, "~> 2.0", only: [:dev], runtime: false},
       {:igniter, "~> 0.6", only: [:dev, :test]},
-        {:commit_hook, path: "~/src/Tool/commit_hook"}
+      {:commit_hook, path: "~/src/Tool/commit_hook"}
     ]
   end
 end
